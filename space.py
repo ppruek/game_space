8fc787a17ea94b45f183093fdb24fd3fbe1f7e82
Arniwatt Chonkiattipoom <arniwatt.c@ku.th>
2017-09-08 14:15:51 +0700

    ship move

--- space.py
+++ space.py
@@ -10,7 +10,10 @@ class SpaceGameWindow(arcade.Window):
         arcade.set_background_color(arcade.color.BLACK)
         self.ship = arcade.Sprite('images/ship.png')
         self.ship.set_position(100, 100)
- 
+    
+    def update(self, delta):
+        self.ship.set_position(self.ship.center_x, self.ship.center_y + 5)
+    
     def on_draw(self):
         arcade.start_render()
         self.ship.draw()
