6069d58f0a29f59fc3918007c02ac42a6ca2c928
Arniwatt Chonkiattipoom <arniwatt.c@ku.th>
2017-09-08 16:24:24 +0700

    hit gold

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 import arcade
+from random import randint
 
 class Model:
     def __init__(self, world, x, y, angle):
@@ -6,6 +7,9 @@ class Model:
         self.x = x
         self.y = y
         self.angle = 0
+
+    def hit(self, other, hit_size):
+        return (abs(self.x - other.x) <= hit_size) and (abs(self.y - other.y) <= hit_size)
 
 class Ship(Model):
     DIR_HORIZONTAL = 0
@@ -37,6 +41,9 @@ class Ship(Model):
 class Gold(Model):
     def __init__(self, world, x, y):
         super().__init__(world, x, y, 0)
+    def random_location(self):
+        self.x = randint(0, self.world.width - 1)
+        self.y = randint(0, self.world.height - 1)
 
 class World:
     def __init__(self, width, height):
@@ -46,7 +53,9 @@ class World:
         self.gold = Gold(self, 400, 400)
 
     def update(self, delta):
-        self.ship.update(delta)    
+        self.ship.update(delta)  
+        if self.ship.hit(self.gold, 15):
+            self.gold.random_location()  
 
     def on_key_press(self, key, key_modifiers):
         if key == arcade.key.SPACE:
