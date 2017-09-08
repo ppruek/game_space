863b330ef93144c1cf4f976d957e3cd4f0c2c177
Arniwatt Chonkiattipoom <arniwatt.c@ku.th>
2017-09-08 15:31:51 +0700

    add class world

--- models.py
+++ models.py
@@ -7,3 +7,14 @@ class Ship:
         if self.y > 600:
             self.y = 0
         self.y += 5
+
+class World:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+ 
+        self.ship = Ship(100, 100)
+ 
+ 
+    def update(self, delta):
+        self.ship.update(delta)        
