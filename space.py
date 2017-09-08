89a61000409a5fb4cea0a7eea85976252650c9dc
Arniwatt Chonkiattipoom <arniwatt.c@ku.th>
2017-09-08 14:12:14 +0700

    add ship

--- space.py
+++ space.py
@@ -13,7 +13,6 @@ class SpaceGameWindow(arcade.Window):
     def on_draw(self):
         arcade.start_render()
  
- 
 if __name__ == '__main__':
     window = SpaceGameWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
     arcade.run()
